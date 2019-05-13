4dcf28a8c239f1d8030d736268270e5ecd83e0b4
Ajaeb Ismael <ajaebabdurahman@outlook.com>
2019-05-13 16:48:34 -0400

    nothing here yet

--- main.js
+++ main.js
@@ -1,18 +1,101 @@
+var aud = document.getElementById("introVideo");
+aud.addEventListener("ended", function() {
+  aud.setAttribute("style","display: none")
+});
 
-type = document.getElementById('type').children
-console.log(type);
 
-function myFunction(i){
-  if (i = 1){
-    type[1].classList.remove('show')
-    type[1].classList.add('hidden')
-    type[0].classList.remove('hidden')
-    type[0].classList.add('show')
-  } else if (i = 2) {
-    type[0].classList.remove('show')
-    type[0].classList.add('hidden')
-    type[1].classList.remove('hidden')
-    type[1].classList.add('show')
 
-  }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // let divs = document.getElementsByTagName('type')
+// // let sections = document.getElementsByTagName('section')
+// // let colors = ["#1abc9c", "#2ecc71", "#f1c40f", "#ecf0f1"]
+//
+// type = document.getElementById('type').children
+// console.log(type);
+// //let part = type.getElementsByTagName('div')
+//
+// function myFunction(i){
+//   if (i = 1){
+//     type[i-1].classList.remove('show')
+//     type[i-1].classList.add('hidden')
+//     type[i].classList.remove('hidden')
+//     type[i].classList.add('show')
+//   } else if (i = 2) {
+//     type[1].classList.remove('show')
+//     type[1].classList.add('hidden')
+//     type[0].classList.remove('hidden')
+//     type[0].classList.add('show')
+//
+//   }
+// }
+//
+// // for (var i = 0; i < sections.length; i++) {
+// //   sections[i].style.backgroundColor = colors[i]
+// // }
+// //
+// // window.addEventListener('scroll', function(){
+// // for (var i in divs) {
+// //   let position = i*800
+// //   if (document.documentElement.scrollTop >= position || document.documentElement.scrollTop < position + 800) {
+// //     divs[i].classList.remove('hidden')
+// //     divs[i].classList.add('show')
+// //   }
+// //   if (document.documentElement.scrollTop < position || document.documentElement.scrollTop > position + 800) {
+// //     divs[i].classList.remove('show')
+// //     divs[i].classList.add('hidden')
+// //   }
+// // }
+// // });
