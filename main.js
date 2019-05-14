7903105116fb9d2fabb94151db43cefd1c883455
Ajaeb Ismael <ajaebabdurahman@outlook.com>
2019-05-13 23:24:38 -0400

    a lot is done i think

--- main.js
+++ main.js
@@ -1,101 +1,24 @@
-var aud = document.getElementById("introVideo");
-aud.addEventListener("ended", function() {
-  aud.setAttribute("style","animation: displayNone")
-});
+// slide
+function currentLayout(n) {
+  showSlides(slideIndex = n);
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // let divs = document.getElementsByTagName('type')
-// // let sections = document.getElementsByTagName('section')
-// // let colors = ["#1abc9c", "#2ecc71", "#f1c40f", "#ecf0f1"]
-//
-// type = document.getElementById('type').children
-// console.log(type);
-// //let part = type.getElementsByTagName('div')
-//
-// function myFunction(i){
-//   if (i = 1){
-//     type[i-1].classList.remove('show')
-//     type[i-1].classList.add('hidden')
-//     type[i].classList.remove('hidden')
-//     type[i].classList.add('show')
-//   } else if (i = 2) {
-//     type[1].classList.remove('show')
-//     type[1].classList.add('hidden')
-//     type[0].classList.remove('hidden')
-//     type[0].classList.add('show')
-//
-//   }
-// }
-//
-// // for (var i = 0; i < sections.length; i++) {
-// //   sections[i].style.backgroundColor = colors[i]
-// // }
-// //
-// // window.addEventListener('scroll', function(){
-// // for (var i in divs) {
-// //   let position = i*800
-// //   if (document.documentElement.scrollTop >= position || document.documentElement.scrollTop < position + 800) {
-// //     divs[i].classList.remove('hidden')
-// //     divs[i].classList.add('show')
-// //   }
-// //   if (document.documentElement.scrollTop < position || document.documentElement.scrollTop > position + 800) {
-// //     divs[i].classList.remove('show')
-// //     divs[i].classList.add('hidden')
-// //   }
-// // }
-// // });
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("layout1");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "grid";
+  dots[slideIndex-1].className += " active";
+}
